00df1600ec7cd51a6a3271770c577f9ef40f4950
linab309 <lin_309@163.com>
2017-04-23 03:24:05 +0800

    add loader

---
+++ Drivers/BSP/STM32L1xx_Nucleo/stm32l1xx_nucleo.h
@@ -0,0 +1,336 @@
+/**
+  ******************************************************************************
+  * @file    stm32l1xx_nucleo.h
+  * @author  MCD Application Team
+  * @version V1.1.0
+  * @date    04-March-2016
+  * @brief   This file contains definitions for:
+  *          - LEDs and push-button available on STM32L1XX-Nucleo Kit 
+  *            from STMicroelectronics
+  *          - LCD, joystick and microSD available on Adafruit 1.8 TFT LCD 
+  *            shield (reference ID 802)
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT(c) 2016 STMicroelectronics</center></h2>
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __STM32L1XX_NUCLEO_H
+#define __STM32L1XX_NUCLEO_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/** @addtogroup BSP
+  * @{
+  */ 
+
+/** @addtogroup STM32L1XX_NUCLEO
+  * @{
+  */ 
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32l1xx_hal.h"
+
+   
+/** @defgroup STM32L1XX_NUCLEO_Exported_Types Exported Types
+  * @{
+  */
+typedef enum 
+{
+  LED_GREEN = 0,
+  LED_RED ,
+  LED_SD ,
+  LED_GPS ,
+  LED_BULE,
+  LED_SURPORT,
+
+  LED_MORE = LED_SURPORT
+} Led_TypeDef;
+
+typedef enum 
+{  
+  BUTTON_USER   = 0,
+  BUTTON_WAKEUP = 1,
+
+  /* Alias */
+  BUTTON_KEY  = BUTTON_WAKEUP
+} Button_TypeDef;
+
+typedef enum 
+{  
+  BUTTON_MODE_GPIO = 0,
+  BUTTON_MODE_EXTI = 1
+} ButtonMode_TypeDef; 
+
+
+
+typedef enum
+{
+  FALSE = 0, TRUE  = !FALSE
+}
+bool;
+
+
+/**
+  * @}
+  */ 
+
+/** @defgroup STM32L1XX_NUCLEO_Exported_Constants Exported Constants
+  * @{
+  */ 
+
+/** 
+  * @brief  Define for STM32L1xx_NUCLEO board  
+  */ 
+#if !defined (USE_STM32L1xx_NUCLEO)
+ #define USE_STM32L1xx_NUCLEO
+#endif
+  
+/** @defgroup STM32L1XX_NUCLEO_LED LED Constants
+  * @{
+  */
+#define LEDn                             6
+
+#define LED_GREEN_PIN                         GPIO_PIN_0
+#define LED_GREEN_GPIO_PORT                   GPIOB
+#define LED_GREEN_GPIO_CLK_ENABLE()           __HAL_RCC_GPIOB_CLK_ENABLE()  
+#define LED_GREEN_GPIO_CLK_DISABLE()          __HAL_RCC_GPIOB_CLK_DISABLE()  
+
+
+#define LED_RED_PIN                         GPIO_PIN_1
+#define LED_RED_GPIO_PORT                   GPIOB
+#define LED_RED_GPIO_CLK_ENABLE()           __HAL_RCC_GPIOB_CLK_ENABLE()  
+#define LED_RED_GPIO_CLK_DISABLE()          __HAL_RCC_GPIOB_CLK_DISABLE()  
+
+#define LED_SD_PIN                         GPIO_PIN_3
+#define LED_SD_GPIO_PORT                   GPIOB
+#define LED_SD_GPIO_CLK_ENABLE()           __HAL_RCC_GPIOB_CLK_ENABLE()  
+#define LED_SD_GPIO_CLK_DISABLE()          __HAL_RCC_GPIOB_CLK_DISABLE()  
+
+#define LED_GPS_PIN                         GPIO_PIN_4
+#define LED_GPS_GPIO_PORT                   GPIOB
+#define LED_GPS_GPIO_CLK_ENABLE()           __HAL_RCC_GPIOB_CLK_ENABLE()  
+#define LED_GPS_GPIO_CLK_DISABLE()          __HAL_RCC_GPIOB_CLK_DISABLE()  
+
+
+#define LED_BULE_PIN                         GPIO_PIN_5
+#define LED_BULE_GPIO_PORT                   GPIOB
+#define LED_BULE_GPIO_CLK_ENABLE()           __HAL_RCC_GPIOB_CLK_ENABLE()  
+#define LED_BULE_GPIO_CLK_DISABLE()          __HAL_RCC_GPIOB_CLK_DISABLE()  
+
+#define LED_SURPORT_PIN                         GPIO_PIN_6
+#define LED_SURPORT_GPIO_PORT                   GPIOB
+#define LED_SURPORT_GPIO_CLK_ENABLE()           __HAL_RCC_GPIOB_CLK_ENABLE()  
+#define LED_SURPORT_GPIO_CLK_DISABLE()          __HAL_RCC_GPIOB_CLK_DISABLE()  
+
+
+
+
+
+
+
+
+#define LEDx_GPIO_CLK_ENABLE(__INDEX__)   do { if((__INDEX__) == 0) LED_GREEN_GPIO_CLK_ENABLE();} while(0)
+#define LEDx_GPIO_CLK_DISABLE(__INDEX__)  (((__INDEX__) == 0) ? LED_GREEN_GPIO_CLK_DISABLE() : 0)
+
+
+/**
+  * @}
+  */ 
+
+/** @defgroup STM32L1XX_NUCLEO_BUTTON BUTTON Constants
+  * @{
+  */  
+#define BUTTONn                          2  
+
+/**
+  * @brief User push-button
+ */
+#define USER_BUTTON_PIN                  GPIO_PIN_15
+#define USER_BUTTON_GPIO_PORT            GPIOA
+#define USER_BUTTON_GPIO_CLK_ENABLE()    __HAL_RCC_GPIOA_CLK_ENABLE()
+#define USER_BUTTON_GPIO_CLK_DISABLE()   __HAL_RCC_GPIOA_CLK_DISABLE()
+#define USER_BUTTON_EXTI_IRQn            EXTI15_10_IRQn
+/* Aliases */
+#define KEY_BUTTON_PIN                   USER_BUTTON_PIN
+#define KEY_BUTTON_GPIO_PORT             USER_BUTTON_GPIO_PORT
+#define KEY_BUTTON_GPIO_CLK_ENABLE()     USER_BUTTON_GPIO_CLK_ENABLE()
+#define KEY_BUTTON_GPIO_CLK_DISABLE()    USER_BUTTON_GPIO_CLK_DISABLE()
+#define KEY_BUTTON_EXTI_IRQn             USER_BUTTON_EXTI_IRQn
+
+#define BUTTONx_GPIO_CLK_ENABLE(__INDEX__)    do { if((__INDEX__) == 0) USER_BUTTON_GPIO_CLK_ENABLE();} while(0)
+#define BUTTONx_GPIO_CLK_DISABLE(__INDEX__)   (((__INDEX__) == 0) ? USER_BUTTON_GPIO_CLK_DISABLE() : 0)
+
+
+/**
+  * @brief User push-button
+ */
+#define WAKEUP_BUTTON_PIN                  GPIO_PIN_13
+#define WAKEUP_BUTTON_GPIO_PORT            GPIOC
+#define WAKEUP_BUTTON_GPIO_CLK_ENABLE()    __HAL_RCC_GPIOC_CLK_ENABLE()
+#define WAKEUP_BUTTON_GPIO_CLK_DISABLE()   __HAL_RCC_GPIOC_CLK_DISABLE()
+#define WAKEUP_BUTTON_EXTI_IRQn            EXTI15_10_IRQn
+/* Aliases */
+#define WAKEUP_KEY_BUTTON_PIN                   WAKEUP_BUTTON_PIN
+#define WAKEUP_KEY_BUTTON_GPIO_PORT             WAKEUP_BUTTON_GPIO_PORT
+#define WAKEUP_KEY_BUTTON_GPIO_CLK_ENABLE()     WAKEUP_BUTTON_GPIO_CLK_ENABLE()
+#define WAKEUP_KEY_BUTTON_GPIO_CLK_DISABLE()    WAKEUP_BUTTON_GPIO_CLK_DISABLE()
+#define WAKEUP_KEY_BUTTON_EXTI_IRQn             WAKEUP_BUTTON_EXTI_IRQn
+
+#define WAKEUP_BUTTONx_GPIO_CLK_ENABLE(__INDEX__)    do { if((__INDEX__) == 1) WAKEUP_BUTTON_GPIO_CLK_ENABLE();} while(0)
+#define WAKEUP_BUTTONx_GPIO_CLK_DISABLE(__INDEX__)   (((__INDEX__) == 1) ? WAKEUP_BUTTON_GPIO_CLK_DISABLE() : 0)
+
+/**
+  * @}
+  */
+
+
+
+/**
+  * @}
+  */
+    
+/** @addtogroup STM32L1XX_NUCLEO_BUS BUS Constants
+  * @{
+  */
+/*###################### SPI1 ###################################*/
+#define NUCLEO_SPIx                                 SPI1
+#define NUCLEO_SPIx_CLK_ENABLE()                    __HAL_RCC_SPI1_CLK_ENABLE()
+
+#define NUCLEO_SPIx_SCK_AF                          GPIO_AF5_SPI1
+#define NUCLEO_SPIx_SCK_GPIO_PORT                   GPIOA
+#define NUCLEO_SPIx_SCK_PIN                         GPIO_PIN_5
+#define NUCLEO_SPIx_SCK_GPIO_CLK_ENABLE()           __HAL_RCC_GPIOA_CLK_ENABLE()
+#define NUCLEO_SPIx_SCK_GPIO_CLK_DISABLE()          __HAL_RCC_GPIOA_CLK_DISABLE()
+
+#define NUCLEO_SPIx_MISO_MOSI_AF                    GPIO_AF5_SPI1
+#define NUCLEO_SPIx_MISO_MOSI_GPIO_PORT             GPIOA
+#define NUCLEO_SPIx_MISO_MOSI_GPIO_CLK_ENABLE()     __HAL_RCC_GPIOA_CLK_ENABLE()
+#define NUCLEO_SPIx_MISO_MOSI_GPIO_CLK_DISABLE()    __HAL_RCC_GPIOA_CLK_DISABLE()
+#define NUCLEO_SPIx_MISO_PIN                        GPIO_PIN_6
+#define NUCLEO_SPIx_MOSI_PIN                        GPIO_PIN_7
+/* Maximum Timeout values for flags waiting loops. These timeouts are not based
+   on accurate values, they just guarantee that the application will not remain
+   stuck if the SPI communication is corrupted.
+   You may modify these timeout values depending on CPU frequency and application
+   conditions (interrupts routines ...). */   
+#define NUCLEO_SPIx_TIMEOUT_MAX                   1000
+
+
+
+
+
+/**
+  * @brief  SD Control Interface pins (shield D4)
+  */
+#define SD_CS_PIN                                 GPIO_PIN_3
+#define SD_CS_GPIO_PORT                           GPIOA
+#define SD_CS_GPIO_CLK_ENABLE()                   __HAL_RCC_GPIOA_CLK_ENABLE()
+#define SD_CS_GPIO_CLK_DISABLE()                  __HAL_RCC_GPIOA_CLK_DISABLE()
+
+/**
+  * @brief  SD Control Lines management
+  */  
+#define SD_CS_LOW()       HAL_GPIO_WritePin(SD_CS_GPIO_PORT, SD_CS_PIN, GPIO_PIN_RESET)
+#define SD_CS_HIGH()      HAL_GPIO_WritePin(SD_CS_GPIO_PORT, SD_CS_PIN, GPIO_PIN_SET)     
+
+
+
+#define BATT_CHARG_OK 0x05
+#define BATT_EMPTY 0x00
+#define BATT_HIGH 0x03
+#define BATT_LOW 0x01
+#define BATT_MID 0x02
+
+#define BATT_CHARGING 0x04
+
+
+
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+    
+/** @addtogroup STM32L1XX_NUCLEO_Exported_Functions
+  * @{
+  */
+uint32_t        BSP_GetVersion(void);
+/** @addtogroup STM32L1XX_NUCLEO_LED_Functions
+  * @{
+  */ 
+
+void            BSP_LED_Init(Led_TypeDef Led);
+void            BSP_LED_DeInit(Led_TypeDef Led);
+void            BSP_LED_On(Led_TypeDef Led);
+void            BSP_LED_Off(Led_TypeDef Led);
+void            BSP_LED_Toggle(Led_TypeDef Led);
+
+
+
+/**
+  * @}
+  */
+
+/** @addtogroup STM32L1XX_NUCLEO_BUTTON_Functions
+  * @{
+  */
+
+void            BSP_PB_Init(Button_TypeDef Button, ButtonMode_TypeDef ButtonMode);
+void            BSP_PB_DeInit(Button_TypeDef Button);
+uint32_t        BSP_PB_GetState(Button_TypeDef Button);
+
+
+/**
+  * @}
+  */
+
+
+
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+  */ 
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __STM32L1XX_NUCLEO_H */
+
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ Src/fatfs.c
@@ -0,0 +1,294 @@
+/**
+  ******************************************************************************
+  * @file   fatfs.c
+  * @brief  Code for fatfs applications
+  ******************************************************************************
+  *
+  * Copyright (c) 2017 STMicroelectronics International N.V. 
+  * All rights reserved.
+  *
+  * Redistribution and use in source and binary forms, with or without 
+  * modification, are permitted, provided that the following conditions are met:
+  *
+  * 1. Redistribution of source code must retain the above copyright notice, 
+  *    this list of conditions and the following disclaimer.
+  * 2. Redistributions in binary form must reproduce the above copyright notice,
+  *    this list of conditions and the following disclaimer in the documentation
+  *    and/or other materials provided with the distribution.
+  * 3. Neither the name of STMicroelectronics nor the names of other 
+  *    contributors to this software may be used to endorse or promote products 
+  *    derived from this software without specific written permission.
+  * 4. This software, including modifications and/or derivative works of this 
+  *    software, must execute solely and exclusively on microcontroller or
+  *    microprocessor devices manufactured by or for STMicroelectronics.
+  * 5. Redistribution and use of this software other than as permitted under 
+  *    this license is void and will automatically terminate your rights under 
+  *    this license. 
+  *
+  * THIS SOFTWARE IS PROVIDED BY STMICROELECTRONICS AND CONTRIBUTORS "AS IS" 
+  * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT 
+  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+  * PARTICULAR PURPOSE AND NON-INFRINGEMENT OF THIRD PARTY INTELLECTUAL PROPERTY
+  * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT 
+  * SHALL STMICROELECTRONICS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+  * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+
+#include "fatfs.h"
+
+uint8_t retUSER;    /* Return value for USER */
+char USER_Path[4];  /* USER logical drive path */
+
+/* USER CODE BEGIN Variables */
+#include "stm32l1xx_nucleo.h"
+
+FATFS SD_FatFs;
+uint8_t  My_Fs_Init(FATFS *SD_FatFs);
+
+/* USER CODE END Variables */    
+
+void MX_FATFS_Init(void) 
+{
+  /*## FatFS: Link the USER driver ###########################*/
+  retUSER = FATFS_LinkDriver(&USER_Driver, USER_Path);
+
+  /* USER CODE BEGIN Init */
+  /* additional user code for init */
+  if(My_Fs_Init(&SD_FatFs) == 1)
+  {
+
+  }  
+  /* USER CODE END Init */
+}
+
+/**
+  * @brief  Gets Time from RTC 
+  * @param  None
+  * @retval Time in DWORD
+  */
+DWORD get_fattime(void)
+{
+  /* USER CODE BEGIN get_fattime */
+  return 0;
+  /* USER CODE END get_fattime */  
+}
+
+/* USER CODE BEGIN Application */
+/* Private functions ---------------------------------------------------------*/
+/**
+  * @brief  Initializes Memory.
+  * @param  None
+  * @retval 0 if operation is successeful, MAL_FAIL else.
+  */
+uint16_t Flash_If_Init(void)
+{ 
+  /* Unlock the internal flash */  
+  HAL_FLASH_Unlock();
+  return 0;
+}
+
+/**
+  * @brief  De-Initializes Memory.
+  * @param  None
+  * @retval 0 if operation is successeful, MAL_FAIL else.
+  */
+uint16_t Flash_If_DeInit(void)
+{ 
+  /* Lock the internal flash */
+  HAL_FLASH_Lock();
+  return 0;
+}
+
+/**
+  * @brief  Erases sector.
+  * @param  Add: Address of sector to be erased.
+  * @retval 0 if operation is successeful, MAL_FAIL else.
+  */
+uint16_t Flash_If_Erase(void)
+{
+  uint32_t NbOfPages = 0;
+  uint32_t PageError = 0;
+  /* Variable contains Flash operation status */
+  HAL_StatusTypeDef status;
+  FLASH_EraseInitTypeDef eraseinitstruct;
+
+   /* Get the number of sector to erase from 1st sector*/
+  NbOfPages = (USBD_DFU_APP_END_ADD - USBD_DFU_APP_DEFAULT_ADD) / FLASH_PAGE_SIZE;
+  eraseinitstruct.TypeErase = FLASH_TYPEERASE_PAGES;
+  eraseinitstruct.PageAddress = USBD_DFU_APP_DEFAULT_ADD;
+  eraseinitstruct.NbPages = NbOfPages;
+  status = HAL_FLASHEx_Erase(&eraseinitstruct, &PageError);
+
+  if (status != HAL_OK)
+  {
+    return 1;
+  }
+  return 0;
+}
+
+/**
+  * @brief  Writes Data into Memory.
+  * @param  src: Pointer to the source buffer. Address to be written to.
+  * @param  dest: Pointer to the destination buffer.
+  * @param  Len: Number of data to be written (in bytes).
+  * @retval 0 if operation is successeful, MAL_FAIL else.
+  */
+uint16_t Flash_If_Write(uint8_t *src, uint8_t *dest, uint32_t Len)
+{
+  uint32_t i = 0;
+  
+  for(i = 0; i < Len; i+=4)
+  {
+    /* Device voltage range supposed to be [2.7V to 3.6V], the operation will
+       be done by byte */ 
+    if(HAL_FLASH_Program(FLASH_TYPEPROGRAM_WORD, (uint32_t)(dest+i), *(uint32_t*)(src+i)) == HAL_OK)
+    {
+     /* Check the written value */
+      if(*(uint32_t *)(src + i) != *(uint32_t*)(dest+i))
+      {
+        /* Flash content doesn't match SRAM content */
+        return 2;
+      }
+    }
+    else
+    {
+      /* Error occurred while writing data in Flash memory */
+      return 1;
+    }
+  }
+  return 0;
+}
+
+/**
+  * @brief  Reads Data into Memory.
+  * @param  src: Pointer to the source buffer. Address to be written to.
+  * @param  dest: Pointer to the destination buffer.
+  * @param  Len: Number of data to be read (in bytes).
+  * @retval Pointer to the physical address where data should be read.
+  */
+uint8_t *Flash_If_Read(uint8_t *src, uint8_t *dest, uint32_t Len)
+{
+  uint32_t i = 0;
+  uint8_t *psrc = src;
+  
+  for(i = 0; i < Len; i++)
+  {
+    dest[i] = *psrc++;
+  }
+  /* Return a valid address to avoid HardFault */
+  return (uint8_t*)(dest); 
+}
+
+
+
+
+uint8_t  My_Fs_Init(FATFS *SD_FatFs)
+{
+
+	uint8_t ret = 0;
+
+
+
+	/* Check the mounted device */
+    if(retUSER == 0)
+    {
+    	if(f_mount(SD_FatFs, (TCHAR const*)USER_Path, 0) != FR_OK)
+    	{
+    	    printf("BSP_SD_INIT_FAILED \r\n");
+    	}  
+    	else
+    	{
+    	    printf("\r\n mkdir_init \r\n");  
+
+        }
+    }
+    else
+    {
+        FATFS_UnLinkDriver(USER_Path);
+    }
+
+    return ret;
+  
+}
+
+
+uint8_t update_frameware(void)
+{
+    FRESULT fr;
+    FIL update_config_fp;
+    uint8_t update_filebuffer[1024];
+    UINT br = 0;
+    uint32_t SectorAddress = USBD_DFU_APP_DEFAULT_ADD;
+    //uint32_t *ramsource = NULL;
+    uint32_t status = 0;
+    static uint8_t led_flag = 0;
+
+    if(f_open(&update_config_fp,(TCHAR const*)"P-1.BIN",FA_READ) == FR_OK)
+    {
+         
+        Flash_If_Init();
+        Flash_If_Erase();
+        while(1)        
+        {
+
+            if(led_flag == 0)
+            {
+                BSP_LED_On(LED_SD);  
+                led_flag++;
+            }
+            else if(led_flag == 1)
+            {
+                BSP_LED_On(LED_GPS);  
+                led_flag++;
+            }
+
+            else if(led_flag == 2)            
+            {
+                BSP_LED_On(LED_SURPORT);  
+                led_flag++;
+            }
+
+            else if(led_flag == 3)
+            {
+                BSP_LED_Off(LED_SD);
+                BSP_LED_Off(LED_GPS);
+                BSP_LED_Off(LED_SURPORT);
+                led_flag = 0;
+            }
+
+            fr = f_read(&update_config_fp,update_filebuffer,1024,&br);
+            //ramsource = (uint32_t*)update_filebuffer;
+            if(br != 0)
+            {
+                status = Flash_If_Write(update_filebuffer,(uint8_t *)SectorAddress,br);
+                printf("file status :%d, %d \r\n",status,br);  
+                if(br <1024)
+                {
+                   printf("file end  \r\n");  
+                   f_close(&update_config_fp);
+                   printf("file end  \r\n");                 
+                   break;
+
+                }
+                SectorAddress += 1024;
+                
+            }
+            else
+            {
+               f_close(&update_config_fp);
+               break;
+            }
+        }
+    }
+
+}
+/* USER CODE END Application */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
